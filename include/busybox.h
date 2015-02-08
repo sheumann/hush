7d8853ba3fee8181e43daaf635e286b98e8ac8fe
Stephen Heumann <heumann1@illinois.edu>
2015-02-07 20:12:55 -0600

    Bump version to 1.1.
    Also bump corresponding BusyBox version to 1.23.1, since it has no relevant changes over 1.23.0.

--- include/busybox.h
+++ include/busybox.h
@@ -5,7 +5,7 @@
 #ifndef BUSYBOX_H
 #define BUSYBOX_H 1
 
-#define BB_VER "1.23.0 (GNO hush 1.0)"
+#define BB_VER "1.23.1 (GNO hush 1.1)"
 
 #include "libbb.h"
 /* BB_DIR_foo and BB_SUID_bar constants: */
